0c93817ede3927ba9844e9ee0610df567713dac6
Dmitry Shmidt <dimitrysh@google.com>
2012-11-02 09:38:42 -0700

    net: wireless: bcmdhd: Avoid suspend on watchdog
    Change-Id: Ic41a8f369a2ee8b2a0084e6a1cbf6b454ff53353
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -5715,7 +5715,8 @@ int dhd_os_check_wakelock(void *dhdp)
 		return 0;
 	dhd = (dhd_info_t *)(pub->info);
 
-	if (dhd && wake_lock_active(&dhd->wl_wifi))
+	if (dhd && (wake_lock_active(&dhd->wl_wifi) ||
+	    wake_lock_active(&dhd->wl_wdwake)))
 		return 1;
 #elif defined(CUSTOMER_HW4) && defined(CONFIG_PM_SLEEP) && defined(PLATFORM_SLP)
 	/* SLP_wakelock_alternative_code */
