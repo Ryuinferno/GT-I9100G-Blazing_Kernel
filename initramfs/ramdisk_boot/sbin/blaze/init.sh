702d18b22412ddcd9177357594b38398dcccafe5
Ryuinferno <ryuinferno.xda@gmail.com>
2013-03-14 11:40:35 +1000

    Custom bootanimation support

---
+++ initramfs/ramdisk_boot/sbin/blaze/init.sh
@@ -0,0 +1,43 @@
+#!/system/bin/sh
+
+# custom bootanimtion support (if no bootanimation.zip is found, will run original animation)
+mount -o rw,remount /system
+if [ -e /system/media/bootanimation.zip ] && [ ! -e /system/etc/cusboot ]; then
+  cp /sbin/blaze/bootanimation_cus /system/bin/bootanimation
+  chmod 755 /system/bin/bootanimation
+  chown 0.2000 /system/bin/bootanimation
+  echo "On" > /system/etc/cusboot
+  mount -o ro,remount /system
+elif [ -e /system/media/bootanimation.zip ] && [ -e /system/etc/cusboot ]; then
+  mount -o ro,remount /system
+elif [ ! -e /system/media/bootanimation.zip ] && [ ! -e /system/etc/cusboot ]; then
+  mount -o ro,remount /system
+else
+  cp /sbin/blaze/bootanimation_ori /system/bin/bootanimation
+  chmod 755 /system/bin/bootanimation
+  chown 0.2000 /system/bin/bootanimation
+  rm /system/etc/cusboot
+  mount -o ro,remount /system
+fi;
+
+# init.d support
+if [ -d /system/etc/init.d ]; then
+  run-parts /system/etc/init.d
+else
+  mount -o rw,remount /system
+  mkdir /system/etc/init.d
+  chmod 777 /system/etc/init.d
+  mount -o ro,remount /system
+fi
+
+if [ -d /data/etc/init.d ]; then
+  run-parts /data/etc/init.d
+else
+  mkdir /data/etc
+  chmod 777 /data/etc
+  mkdir /data/etc/init.d
+  chmod 777 /data/etc/init.d
+fi
+
+
+
