9590ccd3beb21cf76d01ccecce2ef0802b6c75ae
Ryuinferno <ryuinferno.xda@gmail.com>
2013-03-15 13:50:26 +1000

    Custom boot sound support

--- initramfs/ramdisk_boot/sbin/blaze/init.sh
+++ initramfs/ramdisk_boot/sbin/blaze/init.sh
@@ -18,7 +18,30 @@ else
   chown 0.2000 /system/bin/bootanimation
   rm /system/etc/cusboot
   mount -o ro,remount /system
-fi;
+fi
+
+# custom boot sound support
+mount -o rw,remount /system
+if [ -e /system/media/PowerOn.ogg ]; then
+  mv /system/media/PowerOn.ogg /system/media/audio/ui/PowerOn.ogg
+  chmod 644 /system/media/audio/ui/PowerOn.ogg
+  mount -o ro,remount /system
+elif [ -e /system/media/ori_sound ]; then
+  cp /sbin/blaze/PowerOn.ogg /system/media/audio/ui/PowerOn.ogg
+  chmod 644 /system/media/audio/ui/PowerOn.ogg
+  rm /system/media/ori_sound
+  mount -o ro,remount /system
+elif [ -e /system/media/mute ]; then
+  mv /system/media/audio/ui/PowerOn.ogg /system/media/audio/ui/PowerOn.ogg.bak
+  rm /system/media/mute
+  mount -o ro,remount /system
+elif [ -e /system/media/unmute ]; then
+  mv /system/media/audio/ui/PowerOn.ogg.bak /system/media/audio/ui/PowerOn.ogg
+  rm /system/media/unmute
+  mount -o ro,remount /system
+else
+  mount -o ro,remount /system
+fi
 
 # init.d support
 if [ -d /system/etc/init.d ]; then
@@ -38,6 +61,3 @@ else
   mkdir /data/etc/init.d
   chmod 777 /data/etc/init.d
 fi
-
-
-
