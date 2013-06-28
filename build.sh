62d7fa105a91344aca07cf1ff181d24941d2dcda
Ryuinferno <ryuinferno.xda@gmail.com>
2013-06-29 03:13:35 +1000

    Build script improvements and removed CWM5

--- build.sh
+++ build.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# variables
 VERSION="v10"
+TOOLCHAIN="/opt/linaro-4.7.4/bin/arm-unknown-linux-gnueabi-"
+TOOLCHAIN2="/opt/arm-eabi-4.6/bin/arm-eabi-"
 OUTDIR="../out"
 ZIPDIR="../tools/zipfile"
 PLACEHOLDER="Delete_before_compiling"
 ANDROID="initramfs/ramdisk_boot"
 ANDROID2="ramdisk_boot1"
-RECOVERY="ramdisk_recovery"
-RECOVERY_OLD="ramdisk_recovery_old"
-RECOVERY_TOUCH="ramdisk_recovery_touch"
-RECOVERY_MOD="ramdisk_recovery_mod"
-RECOVERY_TWRP="ramdisk_recovery_twrp"
-RECOVERY_PHILZ="ramdisk_recovery_philz"
+REC="ramdisk_recovery"
+REC_TOUCH="ramdisk_recovery_touch"
+REC_MOD="ramdisk_recovery_mod"
+REC_TWRP="ramdisk_recovery_twrp"
+REC_PHILZ="ramdisk_recovery_philz"
 MODULES_EXT=("fs/cifs/cifs.ko" "drivers/samsung/j4fs/j4fs.ko" "net/sunrpc/sunrpc.ko" "net/sunrpc/auth_gss/auth_rpcgss.ko" "fs/nfs/nfs.ko" "fs/lockd/lockd.ko")
 MODULES=("drivers/net/wireless/bcmdhd/dhd.ko" "drivers/scsi/scsi_wait_scan.ko")
 START=$(date +%s)
 
   case "$1" in
   clean)
+          # for a clean build
           make mrproper
           rm -rf ${OUTDIR}
           rm -f ../tools/zipfile/system/lib/modules/cifs.ko
@@ -32,25 +35,25 @@ START=$(date +%s)
    ;;
    *)  
         mkdir -p ${OUTDIR}   
-        make -j8 blazing_defconfig
+        make -j8 blazing_defconfig CROSS_COMPILE=${TOOLCHAIN}
        
         # create modules first to include in ramdisk
-        make -j8 
+        make -j8 CROSS_COMPILE=${TOOLCHAIN}
 
         for module in "${MODULES[@]}" ; do
             cp "${module}" ${ANDROID}/lib/modules
-            cp "${module}" ../tools/zipfile/system/lib/modules
+            cp "${module}" ${ZIPDIR}/system/lib/modules
         done  
         chmod 644 ${ANDROID}/lib/modules/*
         
         for module in "${MODULES_EXT[@]}" ; do
-            cp "${module}" ../tools/zipfile/system/lib/modules
+            cp "${module}" ${ZIPDIR}/system/lib/modules
         done
-        chmod 644 ../tools/zipfile/system/lib/modules/*
+        chmod 644 ${ZIPDIR}/system/lib/modules/*
 
         cd usr/pvr-source/eu*/bu*/li*/om*
-        make -j8 ARCH=arm KERNEL_CROSS_COMPILE=/opt/arm-eabi-4.6/bin/arm-eabi- CROSS_COMPILE=/opt/arm-eabi-4.6/bin/arm-eabi- KERNELDIR=~/Repos/Dual/kernel TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
-        mv ../../../bi*/target/pvrsrvkm_sgx540_120.ko ../../../../../../../tools/zipfile/system/lib/modules
+        make -j8 ARCH=arm CROSS_COMPILE=${TOOLCHAIN2} KERNELDIR=~/Repos/Dual/kernel TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+        mv ../../../bi*/target/pvrsrvkm_sgx540_120.ko ../../../../../../${ZIPDIR}/system/lib/modules
         rm -r ../../../bi*
         cd ../../../../../..
 
@@ -71,27 +74,31 @@ START=$(date +%s)
         echo > system/$PLACEHOLDER
         cd ..
 
-        # create the recovery ramdisk, "cwm6" is for 6.0.1.2, "old" is for 5.5.0.4, "touch" is for touch recovery, "twrp" for TWRP 2.5; default is modified 6.0.3.1
+        # create the recovery ramdisk, "cwm6" is for 6.0.1.2, "old" is for 5.5.0.4, "touch" is for touch recovery, "twrp" for TWRP 2.5, "philz" for Philz recovery
+        # default is modified 6.0.3.1
       case "$1" in
-      old)  
-        rm stage1/recovery.cpio
-        cd ${RECOVERY_OLD}
-        cp recovery.cpio ../stage1/recovery.cpio
-        cd ../..
-      ;;
       touch)
-        rm stage1/recovery.cpio
-        cd ${RECOVERY_TOUCH}
-        rm data/$PLACEHOLDER
-        rm system/bin/$PLACEHOLDER
-        rm tmp/$PLACEHOLDER
-        find . | cpio -o -H newc > ../stage1/recovery.cpio
-        echo > data/$PLACEHOLDER
-        echo > system/bin/$PLACEHOLDER
-        echo > tmp/$PLACEHOLDER
-        cd ../..
+        RECOVERY=${REC_TOUCH}
+        REC_NAME="TOUCH"
       ;;
       cwm6)
+        RECOVERY=${REC}
+        REC_NAME="CWM6"
+      ;;
+      twrp)
+        RECOVERY=${REC_TWRP}
+        REC_NAME="TWRP"
+      ;;
+      philz)
+        RECOVERY=${REC_PHILZ}
+        REC_NAME="PHILZ"
+      ;;
+      *)
+        RECOVERY=${REC_MOD}
+        REC_NAME="CWM6_MOD"
+      ;;
+      esac
+
         rm stage1/recovery.cpio
         cd ${RECOVERY}
         rm data/$PLACEHOLDER
@@ -101,109 +108,30 @@ START=$(date +%s)
         echo > data/$PLACEHOLDER
         echo > system/bin/$PLACEHOLDER
         echo > tmp/$PLACEHOLDER
-        cd ../..
-      ;;
-      twrp)
-        rm stage1/recovery.cpio
-        cd ${RECOVERY_TWRP}
-        rm data/$PLACEHOLDER
-        rm system/bin/$PLACEHOLDER
-        rm tmp/$PLACEHOLDER
-        find . | cpio -o -H newc > ../stage1/recovery.cpio
-        echo > data/$PLACEHOLDER
-        echo > system/bin/$PLACEHOLDER
-        echo > tmp/$PLACEHOLDER
-        cd ../..
-      ;;
-      philz)
-        rm stage1/recovery.cpio
-        cd ${RECOVERY_PHILZ}
-        rm data/$PLACEHOLDER
-        rm system/bin/$PLACEHOLDER
-        rm tmp/$PLACEHOLDER
-        find . | cpio -o -H newc > ../stage1/recovery.cpio
-        echo > data/$PLACEHOLDER
-        echo > system/bin/$PLACEHOLDER
-        echo > tmp/$PLACEHOLDER
         cd ../.. 
-      ;;
-      *)
-        rm stage1/recovery.cpio
-        cd ${RECOVERY_MOD}
-        rm data/$PLACEHOLDER
-        rm system/bin/$PLACEHOLDER
-        rm tmp/$PLACEHOLDER
-        find . | cpio -o -H newc > ../stage1/recovery.cpio
-        echo > data/$PLACEHOLDER
-        echo > system/bin/$PLACEHOLDER
-        echo > tmp/$PLACEHOLDER
-        cd ../.. 
-      ;;
-      esac
         
         # build the zImage
         echo 0 > .version
-        make -j8
+        make -j8 CROSS_COMPILE=${TOOLCHAIN}
         cp arch/arm/boot/zImage ${OUTDIR}
         cp arch/arm/boot/zImage ${ZIPDIR}
      
       cd ..
 
-      case "$1" in
-      old)  
+        # creating zip for kernel
         echo "Creating flashable zip..."
         cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_CWM5.zip *
+        zip -r Blazing_Kernel_${VERSION}_${REC_NAME}.zip *
         cd ..
         echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_CWM5.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_CWM5.zip
-      ;;
-      touch)  
-        echo "Creating flashable zip..."
-        cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_TOUCH.zip *
-        cd ..
-        echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_TOUCH.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_TOUCH.zip
-      ;;
-      cwm6)  
-        echo "Creating flashable zip..."
-        cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_CWM6.zip *
-        cd ..
-        echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_CWM6.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_CWM6.zip
-      ;;
-      twrp)  
-        echo "Creating flashable zip..."
-        cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_TWRP.zip *
-        cd ..
-        echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_TWRP.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_TWRP.zip
-      ;;
-      philz)  
-        echo "Creating flashable zip..."
-        cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_PHILZ.zip *
-        cd ..
-        echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_PHILZ.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_PHILZ.zip
-      ;;
-      *)
-        echo "Creating flashable zip..."
-        cd tools/zipfile
-        zip -r Blazing_Kernel_${VERSION}_CWM6_MOD.zip *
-        cd ..
-        echo "Sigining zip..."
-        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_CWM6_MOD.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_CWM6_MOD.zip
-      ;;
-      esac   
+        java -jar signapk.jar -w testkey.x509.pem testkey.pk8 zipfile/Blazing_Kernel_${VERSION}_${REC_NAME}.zip ${OUTDIR}/Blazing_Kernel_${VERSION}_${REC_NAME}.zip
+  
       rm zipfile/*.zip zipfile/zImage 
       cd ../kernel
    ;;
    esac
 
+# timer
 END=$(date +%s)
 ELAPSED=$((END - START))
 E_MIN=$((ELAPSED / 60))
